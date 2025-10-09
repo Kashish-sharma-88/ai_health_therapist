4470485f034baaab8f3a9e1396cf03f1f67379b1
Falak Naaz <falaknaaz7596@gmail.com>
2025-10-09 18:04:01 +0530

    chatbot

--- Frontend/chat.js
+++ Frontend/chat.js
@@ -30,8 +30,38 @@ async function sendMessage() {
     console.error("Frontend Chat Error:", err);
     chatBox.innerHTML += `<div class="msg bot" style="color:red;"><b>Error:</b> Something went wrong!</div>`;
   }
+  // chatBox.innerHTML += `<div class="msg user"><b>You:</b> ${msg}</div>`;
+saveChat(); // user message ke turant baad
+
+chatBox.innerHTML += `<div class="msg bot"><b>Bot:</b> ${reply}</div>`;
+saveChat(); // bot reply ke turant baad
+
 }
 
 input.addEventListener("keypress", function (e) {
   if (e.key === "Enter") sendMessage();
 });
+function saveChat() {
+  const user = localStorage.getItem("loggedInUser"); // check login user
+  if (user) {
+    localStorage.setItem(`chat_${user}`, chatBox.innerHTML); // chat save karo
+  }
+}
+function loadChat() {
+  const user = localStorage.getItem("loggedInUser");
+  if (user) {
+    const savedChat = localStorage.getItem(`chat_${user}`);
+    if (savedChat) {
+      chatBox.innerHTML = savedChat;
+    }
+  }
+}
+window.onload = loadChat;
+
+function clearChat() {
+  const user = localStorage.getItem("loggedInUser");
+  if (user) {
+    localStorage.removeItem(`chat_${user}`);
+    chatBox.innerHTML = "";
+  }
+}
